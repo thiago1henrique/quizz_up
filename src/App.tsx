a010cde6f7d818fdb25973eb4571806282ff38d5
Thiago <1thiago1henrique@gmail.com>
2025-05-02 14:37:08 -0300

    project init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-dvw h-dvh flex items-center justify-center bg-red-600'>
+        <span>teste div</span>
+      </div>
+    </>
+  )
+}
+
+export default App
